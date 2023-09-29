c1da338c631c1c929f0ba756a3185ba643e4c1ed
zaki <zaki.ilhami46@gmail.com>
2023-09-29 09:20:30 +0700

    comit

---
+++ tipedata.py
@@ -0,0 +1,43 @@
+# mengetahui jenis tipe data
+# int
+a = 20
+print(type(a))
+# float
+b = 5.0000
+print(type(b))
+# str
+c = '20'
+print(type(c))
+# complex
+d = 1 + 2j
+print(type(d))
+
+# string multiline
+
+text = """ ini adalah
+string multiline
+python
+"""
+print(text)
+
+# indexing
+x = 'dicoding'
+print(x[1])
+
+# slicing
+x = 'dicoding'
+print(x[2:])
+
+# variabel
+firstName = 'Zaki'
+lastName = 'Ilhami'
+age = 23
+isMarried = False
+
+# variabel dictionary
+data_diri = {'firstName': 'Zaki', 'lastName': 'Ilhami',
+             'age': 20, 'isMarried': False}
+
+# uppercase
+kata = 'dicoding'
+print(kata.upper())
