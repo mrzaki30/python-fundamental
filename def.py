23db77af9ae132432b6d2c5a834a5e9c36ba3d86
zaki <zaki.ilhami46@gmail.com>
2023-10-06 08:39:22 +0700

    s

---
+++ def.py
@@ -0,0 +1,9 @@
+def mencari_luas_persegi_panjang(lebar, panjang):
+    luas = lebar * panjang
+    return luas
+
+
+persegipanjang_pertama = mencari_luas_persegi_panjang(10, 100)
+print(persegipanjang_pertama)
+persegipanjang_kedua = mencari_luas_persegi_panjang(20, 200)
+print(persegipanjang_kedua)
